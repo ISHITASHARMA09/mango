4d54cc531ca90ea4b67b790521c3b9f95aa59e1d
ISHITASHARMA09 <72793628+ISHITASHARMA09@users.noreply.github.com>
2021-01-20 20:40:20 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# mango
-whjr
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ mangoes.js
@@ -0,0 +1,22 @@
+class Mango{
+  constructor(x,y,r){
+
+  var options={
+      isStatic:true,
+      restitution:0,
+      friction:1.2
+  }
+  this.x=x;
+  this.y=y;
+  this.radius=r;
+  this.image=loadImage("mango.png")
+  this.body=Bodies.circle(this.x, this.y, this.radius, options)
+  World.add(world, this.body);
+}
+display(){
+  var pos =this.body.position;
+  imageMode(CENTER);
+  image(this.image, pos.x, pos.y,this.radius,this.r,)
+  //rect(pos.x, pos.y, this.r);
+}
+}
